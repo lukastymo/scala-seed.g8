c7075166dd0b4cd452f9c33290a7d28b0b26eb3e
Lukasz Tymoszczuk <publicperson@gmail.com>
2020-06-09 11:26:32 +0100

    custom

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,5 +1,42 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
+
+  object Versions {
+    val cats             = "2.1.1"
+    val catsEffect       = "2.1.2"
+    val logback          = "1.2.1"
+    val scalaLogging     = "3.9.2"
+    val newtype          = "0.4.4"
+    val refined          = "0.9.14"
+    val pureConfig       = "0.12.3"
+    val enumeratum       = "1.6.1"
+
+    val scalaTest = "3.1.0"
+  }
+
+  object Libraries {
+    val cats         = "org.typelevel"  %% "cats-core"    % Versions.cats
+    val catsEffect   = "org.typelevel"  %% "cats-effect"  % Versions.catsEffect
+
+    val refinedCore = "eu.timepit" %% "refined"      % Versions.refined
+    val refinedCats = "eu.timepit" %% "refined-cats" % Versions.refined
+
+    val newtype  = "io.estatico"       %% "newtype"        % Versions.newtype
+
+    val pureConfig = "com.github.pureconfig" %% "pureconfig" % Versions.pureConfig
+
+    val logback      = "ch.qos.logback"              % "logback-classic" % Versions.logback
+    val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"   % Versions.scalaLogging
+
+    val enumeratum = "com.beachape" %% "enumeratum" % Versions.enumeratum
+  }
+
+  object TestLibraries {
+    val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % Test
+  }
+
+  object CompilerPlugins {
+  }
+
 }
---
+++ src/main/g8/project/plugins.sbt
@@ -0,0 +1,2 @@
+addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.1.11")
+addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.16")
